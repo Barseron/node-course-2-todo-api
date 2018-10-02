83a3fec5feb3a2e2843df701ad06ce4ebde6bdcc
Starshot Ltd <wilkosbar@gmail.com>
2018-10-02 16:15:00 +0300

    Add tests for POST/user/login

--- server/server.js
+++ server/server.js
@@ -115,8 +115,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-//POST/user/login{email,password}
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
